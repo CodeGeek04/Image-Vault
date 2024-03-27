d4415393e7702daec97d7db2c4f83a202318f305
Shivam Mittal <shivammittal2124@gmail.com>
2024-03-27 11:10:03 +0530

    first commit

--- android/app/src/main/java/com/janvivault/MainApplication.kt
+++ android/app/src/main/java/com/janvivault/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.janvivault
+package com.Imagevault
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/JanviVaultTests/JanviVaultTests.m
+++ ios/JanviVaultTests/JanviVaultTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface JanviVaultTests : XCTestCase
+@interface ImageVaultTests : XCTestCase
 
 @end
 
-@implementation JanviVaultTests
+@implementation ImageVaultTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
